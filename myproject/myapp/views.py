c3bbcd62a749f9591f6e2ae4ae837b787ec6b9ac
wasit7 <wasit7@gmail.com>
2017-07-18 17:15:50 +0700

    almost finished

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-from myapp.forms import CustomerForm
-from myapp.models import Customer
-from myapp.forms import RentForm
-from myapp.models import Rent
+from myapp.forms import CustomerForm, RentForm
+from myapp.models import Customer, Rent, Car
+
 # Create your views here.
 
 def home(request):
@@ -14,9 +13,16 @@ class CreateCustomerView(CreateView):
 	queryset = Customer()
 	template_name='customer.html'
 	form_class = CustomerForm
-	success_url = '/'
+	success_url = '/rent/'
+
 class CreateRentView(CreateView):
 	queryset = Rent()
 	template_name ='rent.html'
 	form_class = RentForm
 	success_url = '/'
+
+class ListCarView(ListView):
+    queryset = Car.objects.all().order_by('-brand')
+    template_name='car_list.html'
+    context_object_name = "car_list"
+    paginate_by = 5
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from myapp import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home , name='home'),
+    #url(r'^$', views.home , name='home'),
     url(r'^customer/$', views.CreateCustomerView.as_view(), name='customer'),
     url(r'^rent/$', views.CreateRentView.as_view(), name='rent'),
+    url(r'^$', views.ListCarView.as_view(), name='car_list'),
     ]
